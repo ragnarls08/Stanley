f0b6a5ae4fc71396365723bee6a630311cbf1583
Ragnar <ragnarls08@ru.is>
2011-02-08 15:59:58 +0000

    added a function that gets json objects from url, and an example function that parses and outputs a dataset

--- stanley.py
+++ stanley.py
@@ -1,1 +1,52 @@
-print "hallo heimur";
+import urllib2
+import simplejson as json
+
+
+urlBase = "http://www.datamarket.com/"
+urlTail = "/api/v1/series.json?callback=&ds=v28&maxresults=10"
+
+fullUrl = urlBase + urlTail 
+
+
+#gets a json timeline from the url given
+#returns a json object
+def getTimeLine( url ):
+	try:
+		request = urllib2.Request(url)
+		response = urllib2.urlopen(request)
+		results = json.load( response )
+		return results[0]
+	except:
+		return -1
+
+
+#prints out the data portion of a timeline as a table
+def displayData( timeLine ):
+	str = ''
+	i = 1 
+	for entry in timeLine["columns"]:
+		str += `i`
+		str += "\t"
+		str += entry["title"].encode('utf-8')
+		str += "\n"
+		i = i+1
+
+	print str	
+	str = ''
+	for x in range(1, i):
+		str += `x`
+		str += "\t" 	
+	print str
+	print 
+	for row in timeLine["data"]:
+		str = ''
+		for item in row:
+			str += `item`
+			str += "\t" 
+		print str	
+
+	
+displayData( getTimeLine( fullUrl ) )
+
+
+	
