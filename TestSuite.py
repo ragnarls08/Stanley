5584b5b320feb40789437cd6c97b865b64c983de
Ragnar <ragnarls08@ru.is>
2011-02-20 22:04:44 +0000

    forgot TestSuite.py

--- TestSuite.py
+++ TestSuite.py
@@ -1,15 +1,17 @@
 import unittest
 
 import DataSetTester
+import TimeLineTester
 
 #create suites for the test classes
 suite1 = DataSetTester.suite()
+suite2 = TimeLineTester.suite()
 
 #initialize suite
 suite = unittest.TestSuite()
  
 #add tests to suite
 suite.addTest(suite1)
-
+suite.addTest(suite2)
 
 unittest.TextTestRunner(verbosity=2).run(suite)
