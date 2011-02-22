65fb442456874d5cf23b6c7d0cbc6dae8a13d659
Arnór Barkarson <arnorb06@ru.is>
2011-02-22 11:10:38 +0000

    MathMagic class added with mean and starard dev functions, tests included

--- TestSuite.py
+++ TestSuite.py
@@ -2,10 +2,12 @@ import unittest
 
 import DataSetTester
 import TimeLineTester
+import MathMagicTester
 
 #create suites for the test classes
 suite1 = DataSetTester.suite()
 suite2 = TimeLineTester.suite()
+suite3 = MathMagicTester.suite()
 
 #initialize suite
 suite = unittest.TestSuite()
@@ -13,5 +15,6 @@ suite = unittest.TestSuite()
 #add tests to suite
 suite.addTest(suite1)
 suite.addTest(suite2)
+suite.addTest(suite3)
 
 unittest.TextTestRunner(verbosity=2).run(suite)
