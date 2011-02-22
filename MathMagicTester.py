4fd4bbc241125a619f8eaf12bc09302e06a0f451
Arnór Barkarson <arnorb06@ru.is>
2011-02-22 11:29:28 +0000

    MathMagic, now actually with the classes ;), PLUS added .gitignore

---
+++ MathMagicTester.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+from MathMagic import MathMagic
+
+class MathMagicTester(unittest.TestCase):
+	
+	def setUp(self):
+		self.testList = [1,2,3,4,5]
+		self.mathMagic = MathMagic()
+		
+	#sends a timeline to assert the standard deviation
+	def testStandardDev(self):
+		self.assertEqual(self.mathMagic.standardDev(self.testList),1.5811388300841898)
+		self.assertEqual(self.mathMagic.standardDev([]),None)
+	#sends a timeline to assrert the mean of that timeline	
+	def testMean(self):
+		self.assertEqual(self.mathMagic.mean(self.testList),3)
+		self.assertEqual(self.mathMagic.mean([]),None)
+		
+		
+def suite():
+	suite = unittest.TestLoader().loadTestsFromTestCase(MathMagicTester)
+	return suite
