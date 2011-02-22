768cf8a99a689a2ce258732222245837bc996427
Thordur Arnarson <thordurba08@ru.is>
2011-02-22 14:36:24 +0000

    Added Notifier.py and NotifierTester.py

--- TestSuite.py
+++ TestSuite.py
@@ -2,10 +2,13 @@ import unittest
 
 import DataSetTester
 import TimeLineTester
+import NotifierTester
 
 #create suites for the test classes
 suite1 = DataSetTester.suite()
 suite2 = TimeLineTester.suite()
+
+suite4 = NotifierTester.suite()
 
 #initialize suite
 suite = unittest.TestSuite()
@@ -13,5 +16,7 @@ suite = unittest.TestSuite()
 #add tests to suite
 suite.addTest(suite1)
 suite.addTest(suite2)
+
+suite.addTest(suite4)
 
 unittest.TextTestRunner(verbosity=2).run(suite)
