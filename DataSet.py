ed8713e700bc88e2aa129c81a21306defd1eb69c
Ragnar <ragnarls08@ru.is>
2011-02-22 16:16:52 +0000

    changes in dataset and timeline

--- DataSet.py
+++ DataSet.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from TimeLine import TimeLine
 
 class DataSet(list):
@@ -9,22 +11,24 @@ class DataSet(list):
 		self.granularity = granularity
 		
 
-	def __len__(self):
-		return len(self.timelines)
 
-	def __repr__(self):
-		return "Title: " + self.title \
-			+ "\nId: " + self.dsId + "\n" \
-			+ "Granularity: " + self.granularity + "\n" \
-			+ "\n".join( str(x) for x in self.timelines )
-
-	def __str__(self):
-		return "Title: " + self.title \
-			+ "\nId: " + self.dsId + "\n" \
-			+ "Granularity: " + self.granularity + "\n" \
-			+ "\n".join( str(x) for x in self.timelines )
+#	def __repr__(self):
+#		return "Title: " + self.title \
+#			+ "\nId: " + self.dsId + "\n" \
+#			+ "Granularity: " + self.granularity + "\n" \
+#			+ "\n".join( str(x) for x in self.timelines )
+#
+#	def __str__(self):
+#		return "Title: " + self.title \
+#			+ "\nId: " + self.dsId + "\n" \
+#			+ "Granularity: " + self.granularity + "\n" \
+#			+ "\n".join( str(x) for x in self.timelines )
 
 
-	def append(self, title, cId):
-		self.timelines.append( TimeLine(title, cId) )
+	def apend(self, item):
+		if not isinstance(item, TimeLine):
+			raise TypeError, 'item is not of type %s' % TimeLine
+		super(DataSet, self).append(item)
 
+#	def append(self, title, cId, data=[]):
+#		self.timelines.append( TimeLine(title, cId, data) )
