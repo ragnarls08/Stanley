3b17988f5c8f3e90450a1e5fd656cae112f07b7f
Ragnar <ragnarls08@ru.is>
2011-04-25 15:57:28 +0000

    testing bollinger

--- MathMagic.py
+++ MathMagic.py
@@ -8,8 +8,11 @@ from scikits.timeseries.lib.moving_funcs import *
 import scipy as sp
 
 class MathMagic:
-  def standardDevAnalysis(self, timeline, timeAxis, frameSize = 60):
-    
+
+	def standardDevAnalysis(self, timeline, timeAxis, frameSize = 60):
+		return iterationTest(timeline, timeAxis)
+
+"""
     #draw moving average, framesize 5, dotted green line
     timeline = timeline.getMaskedArray()
     
@@ -38,5 +41,56 @@ class MathMagic:
 #	flags.append((timeAxis[i],50)) #index of flag + severity of flag
 	
     return flags
+
+"""
+
+
+    def iterationTest(self, timeline, timeAxis):
+		listi = {} 
+
+        listi = iterate(timeline,listi,5)
+        listi = iterate(timeline,listi,10)
+        listi = iterate(timeline,listi,15)
+        listi = iterate(timeline,listi,20)
+
+        listinnokkar = []
+        
+        for key in listi:
+            listinnokkar.append( (key, listi[key]) )
+
+        return listinnokkar 
+
+
+    def iterate(self, timeline, dictionary, frameSize, timeAxis)
+        
+        timeline = timeline.getMaskedArray()
+
+        avg = mov_average_expw(timeline, frameSize)
+        std = mov_std(timeline,frameSize)
+
+        lowerlim = avg-std*2
+        upperlim = avg+std*2
+
+
+        for index, item in enumerate(timeline):
+            if (item < lowerlim[index]):
+                severity = (item - lowerlim[index]) / (item - avg[index])
+                if timeAxis[index] in dictionary:
+                    dictionary[timeAxis[index]] = severity + dictionary[timeAxis[index]] 
+                else:
+                    dictionary[timeAxis[index]] = severity
+
+            elif (item > upperlim[index]):
+                severity = (item - upperlim[index]) / (item - avg[index])
+                if timeAxis[index] in dictionary:
+                    dictionary[timeAxis[index]] = severity + dictionary[timeAxis[index]] 
+                else:
+                    dictionary[timeAxis[index]] = severity
+
+
+
+        
+
+
 
 
