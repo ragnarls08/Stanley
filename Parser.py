3ae1e62d31cc45d7d8de9024b6ab749fac46276b
Ragnar <ragnarls08@ru.is>
2011-05-04 10:28:29 +0000

    more comments

--- Parser.py
+++ Parser.py
@@ -16,24 +16,25 @@ class Parser:
 	def parse(self, dsId, maxResults=0):
 		job = self.gate.getDs(dsId, maxResults)
 
-		#REFACTOR W/THROW EXCEPTION
+		#TODO REFACTOR W/THROW EXCEPTION
 		if not job:
 			print "ble"
 			return None 
-
+		
+		#json object is within a list unwrap it
 		job = job[0]
 	
 		#get data as a list of rows instead of columns	
 		colData = map(None, *job["data"] )
 		
 		dataset = DataSet( job["title"].encode('utf-8'), job["id"].encode('utf-8'), 
-									job["columns"][0]["time_granularity"].encode('utf-8'))
+					job["columns"][0]["time_granularity"].encode('utf-8'))
 		
 
 		for col in range(0, len(colData ) ):
 			tl = TimeLine( job["columns"][col]["title"].encode('utf-8'),
-							job["columns"][col]["cid"].encode('utf-8'),
-							colData[col] )
+					job["columns"][col]["cid"].encode('utf-8'),
+					colData[col] )
 			dataset.append(tl)
 		
 		return dataset				 
